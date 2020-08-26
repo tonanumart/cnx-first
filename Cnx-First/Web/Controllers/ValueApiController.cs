3d8bfc9efb0572f370b2ab31d94a20246c1e7003
cnx-anumart <anumart.c@b-connex.net>
2020-08-26 11:21:14 +0700

    first commit

---
+++ Cnx-First/Web/Controllers/ValueApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    [RoutePrefix("api/value")]
+    public class ValueApiController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new string[]
+            {
+               "test","test2"
+            });
+        }
+    }
+}
